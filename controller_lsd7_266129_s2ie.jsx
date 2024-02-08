09346a430d9f123b7761244e8d80899a89671f79
Shkuropat1 <shkuropyuliia@gmail.com>
2024-02-08 14:47:59 +0200

    Changed configurations while file handling

---
+++ controller_lsd7_266129_s2ie.jsx
@@ -0,0 +1,8 @@
+orange - 19
+
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+
+true + grape
+const getRandomSubset = (array, size) => array.slice(0, size);
+const getUniqueCharacters = str => Array.from(new Set(str)).join("");
+const fetchData = async url => { const response = await fetch(url); return response.json(); }
